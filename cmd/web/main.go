7902605524ca5970376085f4086358bc809b527f
Eric <hayter.eric@gmail.com>
2025-12-15 13:02:16 -0500

    Use environment variables to configure connection to db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -18,36 +18,54 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	// Figure out some better ways to handle this.
-	dsn := flag.String("dsn", "postgres://postgres:mysecretpassword@localhost:5432", "URL to postgis instance")
-	flag.Parse()
+	app := &application{}
+	app.logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	port := app.readEnvVar("SERVER_PORT")
+	addr := fmt.Sprintf(":%s", port)
 
-	db, err := openDB(*dsn)
+	username := app.readEnvVar("POSTGRES_USER")
+	password:= app.readEnvVar("POSTGRES_PASSWORD")
+
+	conn_str := fmt.Sprintf("postgres://%s:%s@postgis:5432", username, password)
+
+
+	db, err := openDB(conn_str)
 	if err != nil {
-		logger.Error(err.Error())
+		app.logger.Error(err.Error())
 		os.Exit(1)
 	}
 	defer db.Close(context.Background())
 
-	app := &application{
-		logger: logger,
-	}
-
-	logger.Info("starting server on", "addr", *addr)
-
-	err = http.ListenAndServe(*addr, app.routes())
-	logger.Error(err.Error())
+	app.logger.Info("starting server on", "addr", addr)
+	err = http.ListenAndServe(addr, app.routes())
+	app.logger.Error(err.Error())
 	os.Exit(1)
 }
 
-func openDB(db_url string) (*pgx.Conn, error) {
-	db, err := pgx.Connect(context.Background(), db_url)
+func openDB(conn_str string) (*pgx.Conn, error) {
+	db, err := pgx.Connect(context.Background(), conn_str)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
 	return db, nil
+}
+
+// Attempts to read a given an environment variable. If the value is not set
+// or empty this function will call os.Exit(1). Therefore, this function should
+// only be used if an environment variable is strictly needed, otherwise, just
+// use os.Getenv.
+func (app *application) readEnvVar(varName string) string {
+	value := os.Getenv(varName)
+	if value == "" {
+		app.logger.Error(fmt.Sprintf("%s is not set", varName))
+		os.Exit(1)
+	}
+	return value
 }
